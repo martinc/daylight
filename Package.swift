aa6fe86334fe13fdd0ff0bff6371c848b118aa72
Chau Thai <chau.thai@gmail.com>
2019-09-16 21:47:56 +0200

    Updated to swift-tools-version:5.1; Removed redundant modifiers

--- Package.swift
+++ Package.swift
@@ -1,5 +1,27 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "Daylight"
+    name: "Daylight",
+    platforms: [
+        .iOS(.v12),
+        .watchOS(.v5),
+        .macOS(.v10_10)
+    ],
+    products: [
+        .library(
+            name: "Daylight",
+            targets: ["Daylight"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "Daylight",
+            dependencies: []),
+        .testTarget(
+            name: "DaylightTests",
+            dependencies: ["Daylight"]),
+    ]
 )
